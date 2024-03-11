f329fcde38b843f12854d720119d3b7c4c75f0d5
Fawaz Alesayi <fawaztsa@gmail.com>
2024-03-11 12:29:51 +0300

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,6 @@
 export * from "./core/crstore.js";
 export { encode, decode } from "./database/operations.js";
-export { group, json, groupJSON } from "./database/json.js";
+export { group, json, groupJSON, JSONPlugin } from "./database/json.js";
 export { primary, crr, index, ordered } from "./database/schema.js";
 export declare const APPEND: any;
 export declare const PREPEND: any;
