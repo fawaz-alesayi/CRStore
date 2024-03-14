a901dedb74e61a4137c729cd6a66ed5c7578dad5
Fawaz Alesayi <fawaztsa@gmail.com>
2024-03-14 13:21:55 +0300

    Update type imports in node.d.ts and node.js

--- runtime/node.d.ts
+++ runtime/node.d.ts
@@ -2,13 +2,12 @@
  * @param {string} file
  * @param {{ binding?: string; extension?: string; }} paths
  * @param {SQLite.Options} options
- * @returns {Promise<{ database: Database, env: "node", }>}
+ * @returns {Promise<{ database: import('better-sqlite3').Database; env: "node"}>}
  */
 export function load(file: string, paths: {
     binding?: string;
     extension?: string;
 }, options?: SQLite.Options): Promise<{
-    database: Database;
+    database: import('better-sqlite3').Database;
     env: "node";
 }>;
-import { Database } from "better-sqlite3";
--- runtime/node.js
+++ runtime/node.js
@@ -1,12 +1,10 @@
 import SQLite from "better-sqlite3";
 import { extensionPath } from "@vlcn.io/crsqlite";
-import { Database } from "better-sqlite3";
-
 /**
  * @param {string} file
  * @param {{ binding?: string; extension?: string; }} paths
  * @param {SQLite.Options} options
- * @returns {Promise<{ database: Database, env: "node", }>}
+ * @returns {Promise<{ database: import('better-sqlite3').Database; env: "node"}>}
  */
 export async function load(file, paths, options = {}) {
   /**
